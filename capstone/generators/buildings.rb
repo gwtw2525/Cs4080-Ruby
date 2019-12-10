71d044867944c9e004f905ac6e300db2e5264bb8
rpabian <47016714+rpabian@users.noreply.github.com>
2019-12-09 19:40:27 -0800

    Created buildings.rb

---
+++ capstone/generators/buildings.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module DBSeeder
+  class Building < Generator
+    class << self
+      def building_names
+        fetch('building.names')
+      end
+
+      def building_room
+        (Array.new(1) { |_| sample(leading) } + 
+            Array.new(2) { |_| sample(digits) }).join
+      end
+
+      def leading
+        %w[1 2 3]
+      end
+
+      def digits
+        %w[0 1 2 3 4 5 6 7 8 9]
+      end
+    end
+  end
+end
